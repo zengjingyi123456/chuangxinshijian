53737655ab96892753385e8ef135aebde7ef9bfa
zrrdeng <2952943743@qq.com>
2020-03-16 21:44:58 +0800

    zan作业

--- zande/Issues.py
+++ zande/Issues.py
@@ -3,43 +3,65 @@ import ssl
 import requests
 import os
 import json
+import time
 
 repos = ['fxsjy/jieba']
 
 def getissues():
-    isu = {}
+    isu = []
+    isutimes=[]
+    titles = []
+    nu=1
     for repo in repos:
-        repo_url = 'https://api.github.com/repos/%s/issues' % repo  # 确定url
-        repoisu = readURL('data/als/%s' % (repo), repo_url)  # 访问url得到数据
-        repoisu = repoisu and json.loads(repoisu)  # 将数据类型转换
-        # 提取想要的信息保存在info中
-        titles = []
-        for issue in repoisu:
-            titles.append(issue['title'])
-        isu =titles
-        print(isu)
-        f=open("text.txt","w+")
-        for l in isu:
-            f.write(l+'\n')
-        f.close()
-        return isu
+        for i in range(17):
+            title=[]
+            isutime=[]
+            repo_url = 'https://api.github.com/repos/fxsjy/jieba/issues?page=%d' %nu# 确定url
+            print(repo_url+'\n')
+            repoisu = readURL('data/als/%s' % (repo), repo_url)  # 访问url得到数据
+            repoisu = repoisu and json.loads(repoisu)  # 将数据类型转换
+            for issue in repoisu:
+                title.append(issue['title'])
+                isutime.append((issue['updated_at'])[:10])
+            f=open("text.txt","a+")
+            ftime=open("uptime.txt","a+")
+            for t in title:
+                f.write(t+'\n')
+            for it in isutime:
+                ftime.write(it+'\n')
+            f.close()
+            ftime.close()
+            print(title)
+            print(isutime)
+            print(nu)
+            nu=nu+1
+            titles=titles+title
+            isutimes=isutimes+isutime
+            time.sleep(10)
+    isu=[titles,isutimes]
+    return isu
 
 #读取url的信息，并建立缓存
+
 def readURL(cache,url):
 	#看看该url是否访问过
-	cache = 'data/cache/%s' % cache
-	if os.path.isfile(cache):
-		with open(cache, 'r') as f:
-			content = f.read()
-		return content
-
 	content = requests.get(url).content.decode()
 
 	#吧文件内容保存下来，以免多次重复访问url，类似于缓存
+	'''
 	folder = cache.rpartition('/')[0]
 	not os.path.isdir(folder) and os.makedirs(folder)
 	with open(cache, 'w') as f:
 		f.write(content)
+	'''
 	return content
-
+	
+    
+'''
+	cache = 'data/cache/%s' % cache
+	if os.path.isfile(cache):
+		with open(cache, 'r') as f:
+			content = f.read()
+    		return content
+        '''
 getissues()
--- zande/alys.py
+++ zande/alys.py
@@ -7,32 +7,23 @@ from jieba import analyse
 import functools
 
 
-# 停用词表加载方法
 def get_stopword_list():
-    # 停用词表存储路径，每一行为一个词，按行读取进行加载
-    # 进行编码转换确保匹配准确率
     stop_word_path = 'stopwords.txt'
     stopword_list = [sw.replace('\n', '') for sw in open(stop_word_path,encoding='utf-8').readlines()]
     return stopword_list
 
 
-# 分词方法，调用结巴接口
 def seg_to_list(sentence, pos=False):
     if not pos:
-        # 不进行词性标注的分词方法
         seg_list = jieba.cut(sentence)
     else:
-        # 进行词性标注的分词方法
         seg_list = psg.cut(sentence)
     return seg_list
 
 
-# 去除干扰词
 def word_filter(seg_list, pos=False):
     stopword_list = get_stopword_list()
     filter_list = []
-    # 根据POS参数选择是否词性过滤
-    ## 不进行词性过滤，则将词性都标记为n，表示全部保留
     for seg in seg_list:
         if not pos:
             word = seg
@@ -42,16 +33,13 @@ def word_filter(seg_list, pos=False):
             flag = seg.flag
         if not flag.startswith('n'):
             continue
-        # 过滤停用词表中的词，以及长度为<2的词
         if not word in stopword_list and len(word) > 1:
             filter_list.append(word)
 
     return filter_list
 
 
-# 数据加载，pos为是否词性标注的参数，corpus_path为数据集路径
 def load_data(pos=False, corpus_path='text.txt'):
-    # 调用上面方式对数据集进行处理，处理后的每条数据仅保留非干扰词
     doc_list = []
     for line in open(corpus_path, 'r',encoding='unicode_escape'):
         content = line.strip()
@@ -62,27 +50,21 @@ def load_data(pos=False, corpus_path='text.txt'):
     return doc_list
 
 
-# idf值统计方法
 def train_idf(doc_list):
     idf_dic = {}
-    # 总文档数
     tt_count = len(doc_list)
 
-    # 每个词出现的文档数
     for doc in doc_list:
         for word in set(doc):
             idf_dic[word] = idf_dic.get(word, 0.0) + 1.0
 
-    # 按公式转换为idf值，分母加1进行平滑处理
     for k, v in idf_dic.items():
         idf_dic[k] = math.log(tt_count / (1.0 + v))
 
-    # 对于没有在字典中的词，默认其仅在一个文档出现，得到默认idf值
     default_idf = math.log(tt_count / (1.0))
     return idf_dic, default_idf
 
 
-#  排序函数，用于topK关键词的按值排序
 def cmp(e1, e2):
     import numpy as np
     res = np.sign(e1[1] - e2[1])
@@ -98,16 +80,14 @@ def cmp(e1, e2):
         else:
             return -1
 
-# TF-IDF类
 class TfIdf(object):
-    # 四个参数分别是：训练好的idf字典，默认idf值，处理后的待提取文本，关键词数量
+
     def __init__(self, idf_dic, default_idf, word_list, keyword_num):
         self.word_list = word_list
         self.idf_dic, self.default_idf = idf_dic, default_idf
         self.tf_dic = self.get_tf_dic()
         self.keyword_num = keyword_num
 
-    # 统计tf值
     def get_tf_dic(self):
         tf_dic = {}
         for word in self.word_list:
@@ -119,7 +99,6 @@ class TfIdf(object):
 
         return tf_dic
 
-    # 按公式计算tf-idf
     def get_tfidf(self):
         tfidf_dic = {}
         for word in self.word_list:
@@ -130,13 +109,11 @@ class TfIdf(object):
             tfidf_dic[word] = tfidf
 
         tfidf_dic.items()
-        # 根据tf-idf排序，去排名前keyword_num的词作为关键词
         for k, v in sorted(tfidf_dic.items(), key=functools.cmp_to_key(cmp), reverse=True)[:self.keyword_num]:
             print(k + "/ ", end='')
         print()
 
 
-# 主题模型
 
 
 
@@ -150,7 +127,6 @@ def tfidf_extract(word_list, pos=False, keyword_num=10):
 def textrank_extract(text, pos=False, keyword_num=10):
     textrank = analyse.textrank
     keywords = textrank(text, keyword_num)
-    # 输出抽取出的关键词
     for keyword in keywords:
         print(keyword + "/ ", end='')
     print()
