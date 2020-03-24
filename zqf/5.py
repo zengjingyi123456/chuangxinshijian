940b4810d84288b31345f4cf5c1c665a9b5f751b
confusedzqf <1351048255@qq.com>
2020-03-24 23:21:52 +0800

    5

---
+++ zqf/5.py
@@ -0,0 +1,149 @@
+import math
+import jieba
+import jieba.posseg as psg
+from gensim import corpora,models
+from jieba import analyse
+import functools
+
+#停用词表加就方法
+def get_stopword_list():
+    stop_word_path = './stopwords.txt'
+    stopword_list = [sw.replace('\n', '') for sw in open(stop_word_path,encoding='utf-8').readlines()]
+    return stopword_list
+
+
+def seg_to_list(sentence, pos=False):
+    if not pos:
+        seg_list = jieba.cut(sentence)
+    else:
+        seg_list = psg.cut(sentence)
+    return seg_list
+
+
+def word_filter(seg_list, pos=False):
+    stopword_list = get_stopword_list()
+    filter_list = []
+    for seg in seg_list:
+        if not pos:
+            word = seg
+            flag = 'n'
+        else:
+            word = seg.word
+            flag = seg.flag
+        if not flag.startswith('n'):
+            continue
+        if not word in stopword_list and len(word) > 1:
+            filter_list.append(word)
+
+    return filter_list
+
+
+def load_data(pos=False, corpus_path='text.txt'):
+    doc_list = []
+    for line in open(corpus_path, 'r',encoding='unicode_escape'):
+        content = line.strip()
+        seg_list = seg_to_list(content, pos)
+        filter_list = word_filter(seg_list, pos)
+        doc_list.append(filter_list)
+
+    return doc_list
+
+
+def train_idf(doc_list):
+    idf_dic = {}
+    tt_count = len(doc_list)
+
+    for doc in doc_list:
+        for word in set(doc):
+            idf_dic[word] = idf_dic.get(word, 0.0) + 1.0
+
+    for k, v in idf_dic.items():
+        idf_dic[k] = math.log(tt_count / (1.0 + v))
+
+    default_idf = math.log(tt_count / (1.0))
+    return idf_dic, default_idf
+
+
+def cmp(e1, e2):
+    import numpy as np
+    res = np.sign(e1[1] - e2[1])
+    if res != 0:
+        return res
+    else:
+        a = e1[0] + e2[0]
+        b = e2[0] + e1[0]
+        if a > b:
+            return 1
+        elif a == b:
+            return 0
+        else:
+            return -1
+
+class TfIdf(object):
+
+    def __init__(self, idf_dic, default_idf, word_list, keyword_num):
+        self.word_list = word_list
+        self.idf_dic, self.default_idf = idf_dic, default_idf
+        self.tf_dic = self.get_tf_dic()
+        self.keyword_num = keyword_num
+
+    def get_tf_dic(self):
+        tf_dic = {}
+        for word in self.word_list:
+            tf_dic[word] = tf_dic.get(word, 0.0) + 1.0
+
+        tt_count = len(self.word_list)
+        for k, v in tf_dic.items():
+            tf_dic[k] = float(v) / tt_count
+
+        return tf_dic
+
+    def get_tfidf(self):
+        tfidf_dic = {}
+        for word in self.word_list:
+            idf = self.idf_dic.get(word, self.default_idf)
+            tf = self.tf_dic.get(word, 0)
+
+            tfidf = tf * idf
+            tfidf_dic[word] = tfidf
+
+        tfidf_dic.items()
+        for k, v in sorted(tfidf_dic.items(), key=functools.cmp_to_key(cmp), reverse=True)[:self.keyword_num]:
+            print(k + "/ ", end='')
+        print()
+
+
+
+
+
+def tfidf_extract(word_list, pos=False, keyword_num=10):
+    doc_list = load_data(pos)
+    idf_dic, default_idf = train_idf(doc_list)
+    tfidf_model = TfIdf(idf_dic, default_idf, word_list, keyword_num)
+    tfidf_model.get_tfidf()
+
+
+def textrank_extract(text, pos=False, keyword_num=10):
+    textrank = analyse.textrank
+    keywords = textrank(text, keyword_num)
+    for keyword in keywords:
+        print(keyword + "/ ", end='')
+    print()
+
+
+if __name__ == '__main__':
+    pos = True
+    ft=open('xyj.txt',"r+",encoding='utf-8')
+    text=''
+    f=ft.readlines()
+    for line in f:
+        text=text+line
+    seg_list = seg_to_list(text, pos)
+    filter_list = word_filter(seg_list, pos)
+
+    print('TF-IDF模型结果：')
+    tfidf_extract(filter_list)
+    print('TextRank模型结果：')
+    textrank_extract(text)
+
+    ft.close()
